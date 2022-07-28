87b2f29c60d66f2678b0f118681f801a1c21cbb0
Luis BC <luinux81@gmail.com>
2022-07-28 12:34:42 +0200

    Añadida paginación opcional y lazy loading de imagenes

--- src/tmpl/default.php
+++ src/tmpl/default.php
@@ -12,63 +12,85 @@ require_once __DIR__ . "/../helper.php";
 
 $lista=Galeria::getURLImagenes($params);
 
+// Si no num positivo entonces no hay paginación
+$paginacion = $params['imagenesPorPagina'] > 0; 
+
 $i=1;
+
 echo "<div class='galeria-outer-wrapper'>
         <h3 class='galeria-header'></h3>
         <p class='galeria-text'></p>
         <div class='galeria-inner-wrapper'>";
         foreach($lista as $l){
-          echo "<div class='galeria-item'>
-                  <div id='galeria-item-preload-" . $i . "' class='galeria-item-preload'>
-                    <img id='item-preload-imagen1-" . $i ."' class='galeria-item-preload-imagen' src='" . JURI::base() . "modules/mod_galeria/css/images/loading-2.gif' />
-                    <img id='item-preload-imagen2-" . $i ."'class='galeria-item-preload-imagen' src='" . JURI::base() . "modules/mod_galeria/css/images/logo-mini-verde.png' />
-                    <img src='" . $l . "' id='galeria-item-imagen-" . $i . "' 
-                          class='galeria-item-imagen' 
-                          data-url='" . $l . "' 
-                          onload=\"imagenCargada(" . $i . ")\" 
-                          onclick=\"popUpImagen(" . $i . ")\" 
-                          />
-                  </div>
-                </div>";
-          if($i==$params['imagenesPorPagina']){
-            break;
+          echo 
+            "<div class='galeria-item'>
+              <div id='galeria-item-preload-" . $i . "' class='galeria-item-preload'>
+                <img 
+                  id='item-preload-imagen1-" . $i ."' 
+                  class='galeria-item-preload-imagen' 
+                  src='" . JURI::base() . "modules/mod_galeria/css/images/loading-2.gif' 
+                />
+
+                <img 
+                  id='item-preload-imagen2-" . $i ."' 
+                  class='galeria-item-preload-imagen' 
+                  src='" . JURI::base() . "modules/mod_galeria/css/images/logo-mini-verde.png' 
+                />
+
+                <img src='" . $l . "' id='galeria-item-imagen-" . $i . "' 
+                  class='galeria-item-imagen' 
+                  data-url='" . $l . "' 
+                  onload=\"imagenCargada(" . $i . ")\" 
+                  onclick=\"popUpImagen(" . $i . ")\" 
+                  loading='lazy'
+                  />
+              </div>
+            </div>";
+          
+          if ($paginacion){
+            if($i==$params['imagenesPorPagina']){
+              break;
+            }
           }
-          else{
+          
+          $i++;
+        }  
+        echo "</div>";
+
+if($paginacion){
+  $i=1;
+  echo "   
+          <div class='galeria-paginacion-wrapper'>";
+          
+          $numPaginas=intdiv(count($lista),$params['imagenesPorPagina']);        
+  
+          while($i<$numPaginas){
+            if($i==1){
+              $clase=" activo";
+            }
+            else{
+              $clase="";
+            }
+            echo "<button class='galeria-paginacion-boton" . $clase ."' onclick='cambiaPagina(" . $i .")'>" . $i . "</button>";
             $i++;
           }
-        }  
-
-$i=1;
-echo "  </div> 
-        <div class='galeria-paginacion-wrapper'>";
-        
-        $numPaginas=intdiv(count($lista),$params['imagenesPorPagina']);        
-
-        while($i<$numPaginas){
-          if($i==1){
-            $clase=" activo";
-          }
-          else{
-            $clase="";
-          }
-          echo "<button class='galeria-paginacion-boton" . $clase ."' onclick='cambiaPagina(" . $i .")'>" . $i . "</button>";
-          $i++;
-        }
-
-$listaNombres=json_encode(Galeria::getNombreImagenes($params));
-
-echo "  <input type='hidden' value='" . $listaNombres . "' id='nombres-imagenes' >  
-        <input type='hidden' value='" . $params['imagenesPorPagina'] . "' id='max-imagenes' >
-        <input type='hidden' value='" . Galeria::getPathImagenes($params) . "' id='ruta-imagenes' >
-        </div>
-        <div class='galeria-overlay ajuste-padding' onclick='cerrarOverlay()'>
-          <div class='galeria-overlay-inner'>
-            <img class='galeria-overlay-imagen' src='' />
-            <div class='galeria-overlay-imagen-social'></div>
+  
+  $listaNombres=json_encode(Galeria::getNombreImagenes($params));
+  
+  echo "  <input type='hidden' value='" . $listaNombres . "' id='nombres-imagenes' >  
+          <input type='hidden' value='" . $params['imagenesPorPagina'] . "' id='max-imagenes' >
+          <input type='hidden' value='" . Galeria::getPathImagenes($params) . "' id='ruta-imagenes' >
           </div>
         </div>
-      </div>
-    ";
+      ";
+}
 
-
+echo "
+  <div class='galeria-overlay ajuste-padding' onclick='cerrarOverlay()'>
+    <div class='galeria-overlay-inner'>
+      <img class='galeria-overlay-imagen' src='' />
+      <div class='galeria-overlay-imagen-social'></div>
+    </div>
+  </div>
+";
 ?>
